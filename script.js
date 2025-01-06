82f4015d76d3724a1db744248f8ca3e119258d07
HumoatStudio <164561146+HumoatStudio@users.noreply.github.com>
2025-01-06 20:27:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//PlusObjectsEditor
+
+function openPlusObjectsEditor() {
+    const PlusObjectsEditorPanel = document.getElementById('PlusObjectsEditorPanel');
+    PlusObjectsEditorPanel.style.display = 'flex'; // Показываем панель
+}
+
+function closePlusObjectsEditor() {
+    const PlusObjectsEditorPanel = document.getElementById('PlusObjectsEditorPanel');
+    PlusObjectsEditorPanel.style.display = 'none'; // Скрываем панель
+}
+
+//SpritesEditor
+
+function openSpritesEditor() {
+    const SpritesEditorPanel = document.getElementById('SpritesEditorPanel');
+    SpritesEditorPanel.style.display = 'flex'; // Показываем панель
+}
+
+function closeSpritesEditor() {
+    const SpritesEditorPanel = document.getElementById('SpritesEditorPanel');
+    SpritesEditorPanel.style.display = 'none'; // Скрываем панель
+}
